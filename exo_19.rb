541f6d0bc4e3f14eecf435bb7332fd71c0f2d47c
Roman Blondel <rblondel23@gmail.com>
2019-10-01 20:32:59 +0200

    fix exo 19

--- exo_19.rb
+++ exo_19.rb
@@ -1,13 +1,15 @@
 i = 1
 nbr = 1
 tab = []
-while nbr < 50
+while nbr <= 50
     if nbr % 2 == 0
         i = sprintf '%02d', nbr
         tab.push "jean.dupont.#{i}@email.fr"
+        puts "jean.dupont.#{nbr}@email.fr"
         nbr = nbr + 1
     else
+        i = sprintf '%02d', nbr
+        tab.push "jean.dupont.#{i}@email.fr"
         nbr = nbr + 1
     end
 end
-puts tab
