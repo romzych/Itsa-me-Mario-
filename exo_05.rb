e484047d96d8c2229dbc5004736616dc376e04c1
Roman Blondel <rblondel23@gmail.com>
2019-10-01 13:15:25 +0200

    5 premier exos

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP"
+puts "Travail : #{10 * 5 * 11}" #le resultat est 550 la ligne multiplie les 3 valeurs
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" #le resultat est 33000 la ligne multiplie les 4 valeurs
+
+puts "Et en secondes ?"
+
+puts 10 * 5 * 11 * 60 * 60 #le resultat est 1980000 la ligne multiplie les 5 valeurs
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts 3 + 2 < 5 - 7 #renvoie un boolean false car 5 > 2
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" #le resultat est 5 la ligne additionne les 2 valeurs
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" #le resultat est -2 la ligne soustrait les 2 valeurs
+
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" #renvoie un boolean true car 5 > -2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #renvoie un boolean true car 5 >= -2
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #renvoie un boolean false car 5 <= -s2
